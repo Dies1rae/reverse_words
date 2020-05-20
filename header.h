e2a726723857c44f42807a15de65aac375cda77d
dies <dies@HOSAKA>
2020-05-20 18:25:24 +0300

    First

---
+++ header.h
@@ -0,0 +1,14 @@
+/*
+ * header.h
+ *
+ *  Created on: Jul 2, 2019
+ *      Author: dies
+ */
+
+#ifndef HEADER_H_
+#define HEADER_H_
+
+void reverse_string(char*);
+void reverse_words(char*);
+
+#endif /* HEADER_H_ */
---
+++ main.c
@@ -0,0 +1,71 @@
+/*
+ * main.c
+ *
+ *  Created on: Jul 2, 2019
+ *      Author: dies
+ */
+#include <stdio.h>
+#include <string.h>
+#include "header.h"
+
+void reverse_string(char*);
+void reverse_words(char*);
+
+int main() {
+  char a[100];
+  gets(a);
+
+  reverse_words(a);
+
+  printf("%s\n", a);
+
+  return 0;
+}
+
+void reverse_words(char *s) {
+  char b[100], *t, *z;
+  int c = 0;
+
+  t = s;
+
+  while(*t) {
+    while(*t != ' ' && *t != '\0') {
+      b[c] = *t;
+      t++;
+      c++;
+    }
+    b[c] = '\0';
+    c = 0;
+
+    reverse_string(b);
+
+    z = b;
+
+    while (*z) {
+      *s = *z;
+      z++;
+      s++;
+    }
+
+    while (*s == ' ') {
+      s++;
+    }
+
+    t = s;
+  }
+}
+void reverse_string(char *t) {
+  int l, c;
+  char *e, s;
+
+  l = strlen(t);
+  e = t + l - 1;
+
+  for (c = 0; c < l/2; c++) {
+    s  = *t;
+    *t = *e;
+    *e = s;
+    t++;
+    e--;
+  }
+}
